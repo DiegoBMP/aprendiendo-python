73321b9ea080a1a8bd084a44ef3fde1e9d1864ac
Diego <Diegomp1539@gmail.com>
2021-08-28 00:17:48 -0400

    tipos de datos

--- datatype.py
+++ datatype.py
@@ -1,5 +1,55 @@
-#Strings
+# Strings
+
 print("hola")
 print('sdsdas')
 print('''que pasa puta''')
 print("""queres que te la ponga""")
+
+print(type("hello wolrd"))
+
+
+# Numbers
+
+# Integer
+print(30)   #int
+
+# Float
+print(30.5) #float
+
+
+# Boolean
+
+True    #bool   
+False
+
+
+# List   
+
+[10, 20, 30, 55]
+['hola', 'bye','queres', 'ahre']
+[10, "hello", True, 10.1]
+
+# Tuples
+
+(10, 20, 30, 55) # esto es inmutable osea no se puede cambiar /tuple
+()
+
+# Dictoniaries
+
+{
+"name":"Ryan",
+"apellido":"Ray",
+"apodo":"fazt"
+}
+
+
+
+
+
+
+
+
+
+
+
+
